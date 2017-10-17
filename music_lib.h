1a75f4288e4af90a7643d3fdae596938450215cb
Helen Ye <00yhelen@gmail.com>
2017-10-17 10:59:22 -0400

    Add music library headers

---
+++ music_lib.h
@@ -0,0 +1,28 @@
+#include "linked_list.h"
+
+// Add songs.
+int add_song(char *, char *);
+
+// Search for a song.
+struct song_node * search_song(char *, char*);
+
+// Search for an artist.
+struct song_node * search_artist(char *);
+
+// Print out all the entries under a certain letter.
+void print_letter(char letter);
+
+// Print out all the songs of a certain artist
+void print_artist(char* artist);
+
+// Print out the entire library.
+void print_lib();
+
+// Shuffle - print out a series of randomly chosen songs.
+void shuffle(int num_songs);
+
+// Delete a song
+void delete_song(char *, char *);
+
+// Delete all the nodes.
+void clear_lib();
